f3055d362e5b55a94baa172bd5587af0c17ab7cd
sokolskiy <vanosocol1@gmail.com>
2025-11-19 02:06:43 +0300

    Initial Java backend. Add App and first endpoint

---
+++ app/src/main/java/java_backend/App.java
@@ -0,0 +1,14 @@
+
+package java_backend;
+
+import java.io.IOException;
+public class App {
+   
+    public static void main(String[] args) throws IOException {
+        System.out.println("start");
+        
+        BackendServer.main(args);
+        
+    }
+    
+}
---
+++ app/src/main/java/java_backend/BackendServer.java
@@ -0,0 +1,30 @@
+package java_backend;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.*;
+import java.net.InetSocketAddress;
+
+public class BackendServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        
+        server.createContext("/api/hello", new HelloHandler());
+        
+        server.start();
+    }
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String response = "Hello my friend!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        }
+    }
+}
+
